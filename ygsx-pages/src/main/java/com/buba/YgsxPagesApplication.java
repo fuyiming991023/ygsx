1ae132ecfd81d14954990609c55ed86f18ade213
Vincent <1220709021@qq.com>
2019-12-19 20:38:35 +0800

    项ç项目主体搭建完成

---
+++ ygsx-pages/src/main/java/com/buba/YgsxPagesApplication.java
@@ -0,0 +1,11 @@
+package com.buba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgsxPagesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YgsxPagesApplication.class,args);
+    }
+}
