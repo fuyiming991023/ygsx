8f7547ea0ee0432e9860abf663b8051e9429f319
Vincent <1220709021@qq.com>
2019-12-25 08:32:49 +0800

    后台管理界面提交

---
+++ ygsx-bgpages/src/main/java/com/buba/YgsxBgPagesApplication.java
@@ -0,0 +1,11 @@
+package com.buba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgsxBgPagesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YgsxBgPagesApplication.class,args);
+    }
+}
---
+++ ygsx-bgpages/src/main/java/com/buba/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.buba.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 设置静态资源访问
+     * @param registry
+     */
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/lib/layui/**").addResourceLocations("classpath:/static/lib/layui/");
+//    super.addResourceHandlers(registry);
+    }
+
+    /**
+     * 设置默认首页面
+     * @param registry
+     */
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:index.html");
+    }
+
+}
